243370f3689657fac79bee9a9e7cf22c3d9869be
bibasayed <sydslyman686@gmail.com.com>
2023-09-25 16:02:03 -0700

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,7 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} 
+}
 else if (k >= 10)
 {
 _putchar(',');
@@ -29,6 +29,6 @@ _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
 }
-_putchar('\n');        
+_putchar('\n');
 }
 }
