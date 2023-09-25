ffda80f99c65896c8b53dfb8776ed0dc7f64f29e
bibasayed <sydslyman686@gmail.com.com>
2023-09-25 16:43:48 -0700

    m

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -29,13 +29,13 @@ aft2 = (aft % l);
 for (i = 92; i < 99; ++i)
 {
 printf(", %lu", aft1 + aft2 / l);
-printf("%lu", aft2 %1);
+printf("%lu", aft2 % 1);
 aft1 = aft1 + bef1;
 bef1 = aft1 - bef1;
 aft2 = aft2 + bef2;
 bef2 = aft2 - bef2;
 }
-}        
+}
 printf("\n");
 return (0);
 }
