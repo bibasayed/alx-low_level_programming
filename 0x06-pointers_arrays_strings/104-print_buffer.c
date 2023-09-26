0afd630f926e0629011f94f1b725ffd734648334
bibasayed <sydslyman686@gmail.com.com>
2023-09-25 18:13:30 -0700

    m

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -25,13 +25,13 @@ if (i < j)
 printf("%02x", *(b + o + i));
 else
 printf("  ");
-}
 if (i % 2)
 {
 printf(" ");
 }
-for (i = 0; i < j; i++)
 }
+for (i = 0; i < j; i++)
+{
 int c = *(b + o + i);
 if (c < 32 || c > 132)
 {
