350ced620778e70dbf86f594809157186e18995f
bibasayed <sydslyman686@gmail.com.com>
2023-09-25 17:34:36 -0700

    m

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,13 +2,13 @@
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
- *
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
 {
 int index = 0;
 while (str[index])
+{
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 if (str[index - 1] == ' ' ||
